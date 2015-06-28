bbaaa155ba518af20dc43d4174cde7859e7d970a
Nadine <nadine.khattak@gmail.com>
2015-06-28 07:37:41 -0400

    update new photo page

--- photos-page/photobehavior.js
+++ photos-page/photobehavior.js
@@ -24,44 +24,90 @@ $(function() {
 		$(this ).effect( "bounce",  { times: 1 },"slow" );
 	});
 
-	/*$(window).load(function() {
-		$(".name-title").show("puff", 1000);
-		$(".photo-title").show("puff", 900);
-		$(".menu-title").show("puff", 800);
-	});
-	
-	$(".name-title").click(function() {	
-		$(this ).effect( "bounce",  { times: 1 },"slow" );
-	});
-	$(".name-title").hover(function() {	
-		$(this ).effect( "bounce", { times: 3 }, "slow" );
-	});
+});
 
-	$(".menu-title").click(function() {	
-		$(this ).effect( "bounce",  { times: 3 },"slow" );
-	});
-	
-	$(".menu-title").click(function() {	
-		$(".nav-menu" ).toggle();
-	});
 
-	$(document).ready(function () {
-    var $bubbles = $('<div class="bubbles" />');
-    for (var i = 0; i < 10; i++) {
-        $bubbles = $('<div class="bubbles" />');
-        $('.design').append($bubbles);
-    }    
-    var $leftbubbles = $('<div class="left-bubbles" />');
-    for (var i = 0; i < 10; i++) {
-        $leftbubbles = $('<div class="left-bubbles" />');
-        $('.left-col').append($leftbubbles);    
+var timerFunction = null;
+var timerFunction1 = null;
+
+    function startAnimation() {
+        if(timerFunction == null) {
+            timerFunction = setInterval(animate, 40);
+        }
+        if(timerFunction1 == null) {
+            timerFunction1 = setInterval(animate, 40);
+        }
     }
 
+    function stopAnimation() {
+        if(timerFunction != null){
+            clearInterval(timerFunction);
+            timerFunction = null;
+        }
+    }
 
-    $(".bubbles").hover(function() {	
-		$(this ).effect( "bounce", { times: 3 }, "slow" );
-	});
-}); */
+    function animate() {
+        var i = 0; 
+        for (var i = 0; i <= 14; i++)
+        {
+        
+        if (i <= 4)
+        {
+        	var circle = document.getElementById("circle" + i);
+        var x = circle.getAttribute("cy");
+        var newX = 2 + parseInt(x);
+        
+        if(newX > 200) {
+            newX = 200;
+            
+            clearInterval(timerFunction);
+            timerFunction = null;
+        }
+           circle.setAttribute("cy", newX);	   
+           
+           
+        }	
+/*
+        if (i > 4 && i <= 9)
+        {
+        var circle = document.getElementById("circle" + i);
+        var x = circle.getAttribute("cx");
+        
+        var newX = 2 + parseInt(x);
+        
+        if(newX > 130) {
+            newX = 80;
+            
+            clearInterval(timerFunction1);
+            timerFunction1 = null;
+        }
+           
+        	circle.setAttribute("cx", newX);	   
+        	
+        }	
+        
+        if (i > 9 && i <= 14)
+        {
+        var circle = document.getElementById("circle" + i);
+        var x = circle.getAttribute("cx");
+        
+        var newX = 2 + parseInt(x);
+        
+        if(newX > 180) {
+            newX = 130;
+            
+            clearInterval(timerFunction1);
+            timerFunction1 = null;
+        }
+           
+        	circle.setAttribute("cx", newX);	   
+        	
+        }	*/
 
+    }
+        	
+        
+       
 
-});
+     
+    }
