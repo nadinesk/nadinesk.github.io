a74f23563dffaf81d2ce0ab24aefdb3299f5744b
Nadine <nadine.khattak@gmail.com>
2016-04-08 00:01:22 -0400

    blah

---
+++ homepage-behavior.js
@@ -0,0 +1,16 @@
+
+
+	$(document).ready(function(){ 
+    $('#location').fadeIn(2000,function(){
+      
+     });    
+
+	$('#name').fadeIn(1000,function(){
+      
+     });    
+
+	$('#github').fadeIn(3000,function(){
+       
+     });  
+});
+
---
+++ test/homepage-behavior.js
@@ -0,0 +1,8 @@
+
+
+	$(document).ready(function(){ 
+    $('#location').fadeIn(2000,function(){
+       $(this).animate({"left" : "300px"}, 4000);
+     });    
+});
+
