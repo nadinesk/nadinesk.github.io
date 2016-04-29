fb6e1b5d313eb563660c9aa8b27922ceb38b0141
Nadine Khattak Fischoff <nadine.khattak@gmail.com>
2016-04-29 08:55:18 -0400

    Update behavior.js

--- newhp/behavior.js
+++ newhp/behavior.js
@@ -5,8 +5,17 @@ $(document).ready(function() {
         var contentlistmain = $("content-list-main");
 
 
-        $("#header-name").width(bodywidth * 0.33);
-        if (bodywidth >=1024)
+      
+
+        if (bodywidth >=1600)
+        {
+          $("#content-list-main").width(bodywidth * 0.25);
+          $("#content-list-main").height(bodywidth * 0.25);
+  $("#header-name").width(bodywidth * 0.25);
+
+        }
+
+        if (bodywidth >=1024 && bodywidth <=1600 )
         {
           $("#content-list-main").width(bodywidth * 0.33);
           $("#content-list-main").height(bodywidth * 0.33);
@@ -14,12 +23,36 @@ $(document).ready(function() {
 
         }
 
-       if (bodywidth < 1024)
+        if (bodywidth >= 780 && bodywidth < 1024)
         {
           $("#content-list-main").width(bodywidth * 0.5);
-          $("#content-list-main").height(bodyheight * .7);
+          $("#content-list-main").height(bodywidth * 0.5);
   $("#header-name").width(bodywidth * 0.5);
 
+        }
+
+        if (bodywidth >=600 && bodywidth < 780)
+        {
+          $("#content-list-main").width(bodywidth * 0.6);
+          $("#content-list-main").height(bodywidth * 0.6);
+        $("#header-name").width(bodywidth * 0.6);
+
+        }
+
+
+        if (bodywidth >=500 && bodywidth < 600 )
+        {
+          $("#content-list-main").width(bodywidth * 0.7);
+          $("#content-list-main").height(bodywidth * 0.7);
+  $("#header-name").width(bodywidth * 0.7);
+
+        }
+
+        if (bodywidth < 500 )
+        {
+          $("#content-list-main").width(bodywidth * 0.8);
+          $("#content-list-main").height(bodywidth * 0.8);
+        $("#header-name").width(bodywidth * 0.8);
 
         }
 
