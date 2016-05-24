68127f881b934c691391a0289f797b0b53ec3088
Nadine <nadine.khattak@gmail.com>
2016-05-24 06:23:30 -0400

    blah

---
+++ New folder/resized3.js
@@ -0,0 +1,11 @@
+<script>
+ var chart = $("#svgtest"),
+    aspect = chart.width() / chart.height(),
+    container = chart.parent();
+$(window).on("resize", function() {
+    var targetWidth = container.width();
+    chart.attr("width", targetWidth);
+    chart.attr("height", Math.round(targetWidth / aspect));
+}).trigger("resize");
+
+</script>
